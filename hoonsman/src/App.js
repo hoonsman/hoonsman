01faa1f306daf5b47a1e9ca1a4a1332e64986bc2
kjeok <kjeok00@gmail.com>
2024-01-08 15:18:38 +0900

    1234

--- hoonsman/src/App.js
+++ hoonsman/src/App.js
@@ -1,15 +1,7 @@
 import './App.css'
-<<<<<<< HEAD
-
-=======
 import Header from './maincomponent/headersection/Header'
 import MainBody from './maincomponent/body/body'
-<<<<<<< HEAD
 import Samples from './maincomponent/samplesection/Samples'
-
-=======
->>>>>>> main
->>>>>>> 8d70d4341794616f9dfad9497a61ac6153b72b22
 function App() {
   return (
     <div className="App">
