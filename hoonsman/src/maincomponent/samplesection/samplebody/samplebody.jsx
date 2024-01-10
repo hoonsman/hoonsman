d8425b0f2c642cf35b3a23c2650f62da48a517c5
kjeok <kjeok00@gmail.com>
2024-01-10 11:05:09 +0900

    ~templatePrac

--- hoonsman/src/maincomponent/samplesection/samplebody/samplebody.jsx
+++ hoonsman/src/maincomponent/samplesection/samplebody/samplebody.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Style from './samplebody.module.css'
 import Container1 from './container1/container1'
+import Container2 from './container2/container2'
+import Container3 from './container3/container3'
 
 const SampleBody = () => {
+  const [scrollPosition, setScrollPosition] = useState(0)
+
+  const handleScroll = () => {
+    const position = window.pageYOffset
+    setScrollPosition(position)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <div className={Style.container}>
+      <div style={{ color: scrollPosition > 100 ? 'red' : 'black' }}>
+        스크롤되면 색이 변합니다!
+      </div>
       <Container1 />
+      <Container2 />
+      <Container3 />
     </div>
   )
 }
