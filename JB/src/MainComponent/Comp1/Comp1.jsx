5d02aa26dae6556b0d9ac5c885c87663259b8d00
kjeok <kjeok00@gmail.com>
2024-01-15 00:38:59 +0900

    Sample!

--- JB/src/MainComponent/Comp1/Comp1.jsx
+++ JB/src/MainComponent/Comp1/Comp1.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Style from './Comp1.module.css'
 
-const Comp1 = ({ active }) => {
+const Comp1 = ({ active, showMessage, showDetailImage }) => {
+  const messageClass = showMessage ? Style.fadeIn : ''
+  const detailImageClass = showDetailImage ? Style.fadeIn : ''
+
   return (
-    <div
-      className={`${Style.container} ${active ? Style.active : Style.inactive}`}
-    >
-      <div className={Style.message}>Hi nice to meet you!</div>
+    <div className={Style.container}>
+      <div className={`${Style.message} ${messageClass}`}>갤럭시 Z 플립 5</div>
       <div className={Style.imageContainer}>
-        <img src={`${process.env.PUBLIC_URL}/image1.png`} alt="Image" />
+        <img src={`${process.env.PUBLIC_URL}/s_image1.png`} alt="Image" />
+      </div>
+      <div className={`${Style.detailImage} ${detailImageClass}`}>
+        <img src={`${process.env.PUBLIC_URL}/detailImage.png`} alt="Detail" />
       </div>
     </div>
   )
--- JB/src/MainComponent/Main.jsx
+++ JB/src/MainComponent/Main.jsx
@@ -3,32 +3,59 @@ import styles from './Main.module.css'
 import Comp1 from './Comp1/Comp1'
 import Comp2 from './Comp2/Comp2'
 import Comp3 from './Comp3/Comp3'
+import Comp4 from './Comp4/Comp4'
+import Blank from './Blank/Blank'
 
 const Main = () => {
-  // activeComp 상태를 사용하여 현재 활성화된 컴포넌트를 추적합니다.
   const [activeComp, setActiveComp] = useState('Comp1')
+  const [showMessage1, setShowMessage1] = useState(false)
+  const [showMessage2_1, setShowMessage2_1] = useState(false)
+  const [showMessage2_2, setShowMessage2_2] = useState(false)
+  const [showMessage3_1, setShowMessage3_1] = useState(false)
+  const [showMessage3_2, setShowMessage3_2] = useState(false)
+  const [showMessage4, setShowMessage4] = useState(false)
 
-  // containerRef는 스크롤 이벤트를 위한 DOM 참조입니다.
+  const [showDetailImage, setShowDetailImage] = useState(false)
+  const [showImage1, setShowImage1] = useState(false)
+  const [showImage2, setShowImage2] = useState(false)
+  const [showImage3, setShowImage3] = useState(false)
+
   const containerRef = useRef(null)
 
   const handleScroll = () => {
-    // 현재 스크롤 위치와 컨테이너 높이를 가져옵니다.
     const scrollY = containerRef.current.scrollTop
     const height = containerRef.current.clientHeight
 
-    // 스크롤 위치에 따라 활성 컴포넌트를 변경합니다.
+    setShowMessage1(scrollY >= height * 1.5 && scrollY < height * 3)
+    setShowDetailImage(scrollY >= height * 2 && scrollY < height * 3)
+    setShowMessage2_1(scrollY >= height * 3.5 && scrollY < height * 5)
+    setShowMessage2_2(scrollY >= height * 4 && scrollY < height * 5)
+    setShowMessage3_1(scrollY >= height * 5.5 && scrollY < height * 9)
+    setShowMessage3_2(scrollY >= height * 6 && scrollY < height * 9)
+    setShowMessage4(scrollY >= height * 9.5 && scrollY < height * 13)
+
+    setShowImage1(scrollY >= height * 7)
+    setShowImage2(scrollY >= height * 7.5)
+    setShowImage3(scrollY >= height * 8)
+
     if (scrollY < height) {
-      setActiveComp('Comp1') // 스크롤이 첫 번째 컴포넌트 범위에 있을 때
+      setActiveComp('Blank')
     } else if (scrollY < height * 3) {
-      setActiveComp('Comp2') // 스크롤이 두 번째 컴포넌트 범위에 있을 때
+      setActiveComp('Comp1')
     } else if (scrollY < height * 5) {
-      setActiveComp('Comp3') // 스크롤이 세 번째 컴포넌트 범위에 있을 때
+      setActiveComp('Comp2')
+    } else if (scrollY < height * 9) {
+      setActiveComp('Comp3')
+    } else if (scrollY < height * 13) {
+      setActiveComp('Comp4')
+    } else {
+      setActiveComp('Blank')
     }
   }
 
-  // 컴포넌트가 마운트될 때 스크롤 이벤트 리스너를 추가하고, 언마운트될 때 제거합니다.
   useEffect(() => {
     const container = containerRef.current
+    container.style.height = '100vh'
     container.addEventListener('scroll', handleScroll)
 
     return () => {
@@ -36,23 +63,47 @@ const Main = () => {
     }
   }, [])
 
-  // 현재 활성 컴포넌트에 따라 애니메이션 클래스를 결정합니다.
   const getAnimationClass = (compName) => {
-    // 활성 컴포넌트일 경우 fadeIn, 아닐 경우 fadeOut 클래스를 적용합니다.
     return activeComp === compName ? styles.fadeIn : styles.fadeOut
   }
 
   return (
     <div className={styles.container} ref={containerRef}>
-      {/* 각 컴포넌트에 애니메이션 클래스를 적용합니다. */}
       <div className={`${styles.mainContainer} ${getAnimationClass('Comp1')}`}>
-        <Comp1 active={activeComp === 'Comp1'} />
+        <Comp1
+          active={activeComp === 'Comp1'}
+          showMessage={showMessage1}
+          showDetailImage={showDetailImage}
+        />
       </div>
       <div className={`${styles.mainContainer} ${getAnimationClass('Comp2')}`}>
-        <Comp2 active={activeComp === 'Comp2'} />
+        <Comp2
+          active={activeComp === 'Comp2'}
+          showMessage1={showMessage2_1}
+          showMessage2={showMessage2_2}
+        />
       </div>
       <div className={`${styles.mainContainer} ${getAnimationClass('Comp3')}`}>
-        <Comp3 active={activeComp === 'Comp3'} />
+        <Comp3
+          active={activeComp === 'Comp3'}
+          showMessage1={showMessage3_1}
+          showMessage2={showMessage3_2}
+          showImage1={showImage1}
+          showImage2={showImage2}
+          showImage3={showImage3}
+        />
+      </div>
+      <div className={`${styles.mainContainer} ${getAnimationClass('Comp4')}`}>
+        <Comp4 // Comp4 컴포넌트 추가
+          active={activeComp === 'Comp4'}
+          showMessage4={showMessage4}
+        />
+      </div>
+      <div className={`${styles.mainContainer} ${getAnimationClass('Blank')}`}>
+        <Blank />
+      </div>
+      <div className={`${styles.mainContainer} ${getAnimationClass('Blank')}`}>
+        <Blank />
       </div>
     </div>
   )
