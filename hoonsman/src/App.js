421c5f504ea35b469ca7e6d328dca6f71e82bfcb
김재형 <Hiruca3220@gmail.com>
2024-01-18 15:54:29 +0900

    로고 메인 수정

--- hoonsman/src/App.js
+++ hoonsman/src/App.js
@@ -1,12 +1,16 @@
 import './App.css'
 import Header from './maincomponent/headersection/Header'
 import MainBody from './maincomponent/body/body'
-import SeminarSample from './maincomponent/samples/Seminar/seminarsample'
 
 function App() {
   return (
     <div className="App">
-      <SeminarSample size={{ width: '1020px', height: '800px' }} />
+      <div>
+        <Header />
+      </div>
+      <div>
+        <MainBody />
+      </div>
     </div>
   )
 }
