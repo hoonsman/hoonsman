b27411116c11c6dd5715eb78356ec4a2f1be146a
김재형 <Hiruca3220@gmail.com>
2024-01-18 13:53:53 +0900

    샘플 id= 1 사이즈 변환 완

--- hoonsman/src/App.js
+++ hoonsman/src/App.js
@@ -1,16 +1,12 @@
 import './App.css'
 import Header from './maincomponent/headersection/Header'
 import MainBody from './maincomponent/body/body'
+import SeminarSample from './maincomponent/samples/Seminar/seminarsample'
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Header />
-      </div>
-      <div>
-        <MainBody />
-      </div>
+      <SeminarSample size={{ width: '1020px', height: '800px' }} />
     </div>
   )
 }
